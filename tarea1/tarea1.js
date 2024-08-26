07fd18041b5c0e9e79bb63aa0400a936f269b5ae
Rafael Romero <rafa710@gmail.com>
2024-08-25 23:16:06 -0300

    desarrolla la tarea 1

---
+++ tarea1/tarea1.js
@@ -0,0 +1,8 @@
+const $botonCalcular = document.querySelector("#btn-calcular");
+$botonCalcular.onclick = function(){
+  const $salarioAnual = document.querySelector("#salario-anual").value;
+  const MESES_EN_UN_ANIO = 12;
+  const $salarioMensual = document.querySelector('#salario-mensual');
+  $salarioMensual.value = $salarioAnual / MESES_EN_UN_ANIO;
+  return false;
+}
